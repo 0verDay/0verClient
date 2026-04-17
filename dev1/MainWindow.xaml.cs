5cba47ab9ac24f632424774900f7e56d0b043396
yu <yy1973100@163.com>
2026-04-17 20:42:42 +0800

    init and update dev1
    初始化并添加dev1版本

---
+++ dev1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace _0verClient;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ButtonState == MouseButtonState.Pressed)
+        {
+            DragMove();
+        }
+    }
+
+    private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void MainBorder_MouseEnter(object sender, MouseEventArgs e)
+    {
+        mainBorder.BorderBrush = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.White);
+    }
+
+    private void MainBorder_MouseLeave(object sender, MouseEventArgs e)
+    {
+        mainBorder.BorderBrush = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(68, 68, 68));
+    }
+}
---
+++ dev1/run.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "%~dp0"
+start "" "bin\Debug\net10.0-windows\0verClient.exe"
